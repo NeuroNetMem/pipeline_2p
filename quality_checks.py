8200899b2f0d4d29c3d530b119956b411c866825
davidespalla <spalladavide.92@gmail.com>
2023-02-15 10:23:20 +0100

    added visualization and other

--- quality_checks.py
+++ quality_checks.py
@@ -15,7 +15,13 @@ saturation_th = - 100
 log_file = output_path + '/log.txt'
 
 
-tif_files = ut.make_tif_list(xls_info_file,animals)
+#tif_files = ut.make_tif_list(xls_info_file,animals)
+
+tif_files = ['/ceph/imaging1/arie/429420_toms/20230203_429420/20230203_429420_00002.tif', 
+             '/ceph/imaging1/arie/429420_toms/20230202_429420/20230202_429420_00001.tif',
+             '/ceph/imaging1/arie/429419_croc/20230202_429419/20230202_429419_00002.tif',
+             '/ceph/imaging1/arie/429419_croc/20230202_429419/20230202_429419_00002.tif'
+            ]
 
 Path(output_path).mkdir(parents=True, exist_ok=True)
 
@@ -26,10 +32,7 @@ for i,filepath in enumerate(tif_files):
 
     #check if file exists, if not save in txt
     if not Path(filepath).is_file():
-        print(f'Not found : {filepath}, logging and skipping')
-        f = open(log_file,'w')
-        f.write(f'file {filepath} not found\n')
-        f.close()
+        print(f'Not found : {filepath},skipping')
         continue
 
 
@@ -46,6 +49,7 @@ for i,filepath in enumerate(tif_files):
 
     print('loading movie ...')
     movie = cm.load(filepath)
+    print(f'movie shape: {movie.shape}')
 
     print('computing quality measures ...')
     movie_min = np.min(np.asarray(movie).flatten())
@@ -61,20 +65,45 @@ for i,filepath in enumerate(tif_files):
     data_quality['max_val'].append(movie_max)
 
     
-    print('making plots ...')
-
+    print('making timecourse plot ...')
+    
+    # AVERAGE ACTIVITY TIMECOURSE
+     
     plt.figure(figsize=(15,5))
     plt.plot(mean_activity)
     plt.title(f'saturation fraction: {saturation_frac}')
     plt.xlabel('time (frames)')
     plt.ylabel('average pixel value')
     plt.savefig(save_path+f'/{video_name}_luminance_timecourse.png')
+    
+    # SUMMARY IMAGES TIMECOURSE
+    
+    print('computing mean image ...')
+    mean_image = np.mean(movie,axis=0)
 
-
-    mean_img = np.mean(movie,axis=0)
+    
     plt.figure(figsize=(10,10))
-    plt.imshow(mean_img,cmap='gray')
+    plt.title('Mean image')
+    plt.imshow(mean_image,cmap='gray')
     plt.savefig(save_path+f'/{video_name}_mean_img.png')
+    
+    #print('compute correlation adn pnr images ...')
+    #correlation_image,pnr_image = cm.summary_images.correlation_pnr(movie,swap_dim=False)
+    
+    #print('making summary images plot ...')
+    
+    #plt.subplot(1,2,1)
+    #plt.title('Corr image')
+    #plt.imshow(correlation_image,cmap='gray')
+    
+    #plt.subplot(1,2,2)
+    #plt.title('PNR image')
+    #plt.imshow(pnr_image,cmap='gray')
+    
+    #plt.tight_layout()
+    
+    
+    #plt.savefig(save_path+f'/{video_name}_summary_imgs.png')
 
 
     # clear memory
